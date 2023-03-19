62db321ceb76192f8ba975ec9d8a1f4424fdb23c
sanjay5649 <stuwrkr-smaru7@mx.uillinois.edu>
2023-03-19 05:33:15 +0000

    user details looks good

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,14 @@ Rails.application.routes.draw do
 
   get("/users/:path_username", { :controller => "users", :action => "show"})
 
+  get("/photos", { :controller => "photos", :action => "index" })
 
-end
+  get("/photos/:path_id", { :controller =>"photos", :action =>"show" })
+
+  get("/delete_photo/:toast_id" , { :controller =>"photos", :action=>"baii" })
+
+  get("/insert_photo", { :controller =>"photos", :action =>"create" })
+
+  get("/update_photo/:modify_id", {:controller => "photos", :action => "update" })
+
+  end
