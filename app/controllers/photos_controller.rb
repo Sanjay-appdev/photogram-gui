b40fa80291fcfd6fa1f48a558dd2f3ef3850d379
sanjay5649 <stuwrkr-smaru7@mx.uillinois.edu>
2023-03-19 21:45:15 +0000

    fifty fourty seven

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -34,9 +34,50 @@ class PhotosController < ApplicationController
   end
 
   def create
+    # Parameters: {"query_image"=>"https://www.chicagobooth.edu/-/media/project/chicago-booth/why-booth/a-global-footprint/a-dynamic-influence-in-europose-the-middle-east-and-africa/chicago-booth-london-building-entrance.jpg?cx=0.58&cy=0.72&cw=940&ch=749&hash=BA83D00D9BE1FDAA565CB2B6B949C1D0", "query_caption"=>"chicago Booth", "query_owner_id"=>"117"}
 
-    render({ :template => "photo_templates/create.html.erb" })
 
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
 
-  end
-end
+     a_new_photo = Photo.new
+
+     a_new_photo.image = input_image
+     a_new_photo.caption = input_caption
+     a_new_photo.owner_id = input_owner_id
+
+     a_new_photo.save
+
+    #render({ :template => "photo_templates/create.html.erb" })
+
+    next_url = "/photos/" + a_new_photo.id.to_s
+
+    redirect_to(next_url)
+    end
+
+    def update
+      #Parameters: {"query_image"=>"https://www.chicagobooth.edu/-/media/project/chicago-booth/why-booth/a-global-footprint/a-dynamic-influence-in-europose-the-middle-east-and-africa/chicago-booth-london-building-entrance.jpg?cx=0.58&cy=0.72&cw=940&ch=749&hash=BA83D00D9BE1FDAA565CB2B6B949C1D0", "query_caption"=>"Booth Upated", "modify_id"=>"955"}
+
+      the_id = params.fetch("modify_id")
+
+      matching_photos = Photo.where({ :id => the_id })
+
+      the_photo = matching_photos.at(0)
+
+      input_image = params.fetch("query_image")
+      input_caption = params.fetch("query_caption")
+
+      the_photo.image = input_image 
+      the_photo.caption = input_caption
+
+      the_photo.save
+      
+      #render({ :template => "photo_templates/update.html.erb" })
+
+      next_url = "/photos/" + the_photo.id.to_s
+
+      redirect_to(next_url)
+
+    end
+    end
